27e593d78645eb4c868c4c993ba95aa1a42e80d3
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2016-01-29 21:09:49 -0600

    removed validation to stop the complain

--- scripts/start-etcd-app.sh
+++ scripts/start-etcd-app.sh
@@ -2,5 +2,5 @@
 
 echo "Starting etcd service and pod for applications"
 
-kubectl create -f ../etcd/etcd-service.json
+kubectl create --validate=false -f ../etcd/etcd-service.json
 kubectl create -f ../etcd/etcd-pod.json
