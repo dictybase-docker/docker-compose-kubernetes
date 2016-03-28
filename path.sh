a41d3ec6653e720e1db78c7228b6e35fb02a6beb
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2016-03-28 10:37:14 -0500

    replaced with absolute path

--- path.sh
+++ path.sh
@@ -1,1 +1,1 @@
-export PATH=${PWD}/kubernetes-v1.2.0/_output/dockerized/bin/linux/amd64:${PATH}
+export PATH=~/Projects/docker-compose-kubernetes/kubernetes-v1.2.0/_output/dockerized/bin/linux/amd64:${PATH}
